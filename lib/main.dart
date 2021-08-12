4ea7f2078281fb52adce0f0ca1892cf28682844b
Judson Henrique <judson.henrique01@gmail.com>
2021-08-12 19:31:02 -0300

    config project

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:pokelandia/views/root/root.dart';
+
+void main() => runApp(RootApp());
---
+++ lib/views/root/root.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class RootApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xff015FEB),
+      ),
+      // home:
+    );
+  }
+}
